70caeb3650f2ca8e80eaf04bc6b4c7e46c7546bb
Lara Namiq <larasnamiq@gmail.com>
2022-08-11 19:58:25 +0100

    Add example tests

--- app/index.js
+++ app/index.js
@@ -1,1 +1,8 @@
 export const add2Nums = (num1, num2) => num1 + num2;
+
+
+export const greet = (name = "Anonymous") =>  `Hello, ${name}!`;
+
+export const calculateTip = (billTotal, tipPercent = 20) => 
+    (billTotal * tipPercent) / 100;
+
--- app/index.test.js
+++ app/index.test.js
@@ -1,5 +1,29 @@
-import { add2Nums } from "./index.js";
+import { add2Nums, greet, calculateTip } from "./index.js";
 
 test("1 + 1 = 2", () => {
   expect(add2Nums(1, 1)).toBe(2);
+});
+
+describe("greet", () => {
+  // Happy path test
+  test("greets with the correct name", () => {
+    expect(greet("John")).toBe("Hello, John!");
+    expect(greet("Jane")).toBe("Hello, Jane!");
+  });
+
+  // Sad path test
+  test("greets anonymous", () => {
+    expect(greet()).toBe("Hello, Anonymous!");
+  });
+});
+
+describe("calculateTip", () => {
+  it("calculates the correct tip", () => {
+    expect(calculateTip(100, 10)).toBe(10);
+    expect(calculateTip(100, 20)).toBe(20);
+  });
+
+  it("calculates with a default tip of 20%", () => {
+    expect(calculateTip(100)).toBe(20);
+  })
 });
